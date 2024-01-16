061810fbedb8ff678bdbe627506d195fd2538972
okeke christian <chrismalchima@gmail.com>
2024-01-16 12:35:45 -0800

    commit message

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,21 @@
-/*
- * File: 1-print_binary.c
- * Auth: Okeke Christian Chima
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - prints a number in binary
+ * @n: the integer
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-
-	_putchar((n & 1) + '0');
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
